5ade46b70cfc455f0870b32f3ee074e44239e23f
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2024-10-02 12:06:15 -0700

    Update tabcloaker.js

--- tabcloaker.js
+++ tabcloaker.js
@@ -20,5 +20,20 @@ function Help() {
     console.log("Favicon updated to:", faviconUrl);
 }
 
+function Help1() {
+    var t = localStorage.getItem("TabText");
 
+    if (x && x !== "null") {
+        document.title = t; // Set title to the value from localStorage
+    } else {
+        document.title = "Clever | Portal"; // Set default title if TabText doesn't exist or is "null"
+    }
 
+    console.log("Title set to: " + document.title);
+
+    // Remove any existing favicon
+    var existingLink = document.querySelector('link[rel="icon"], link[rel="shortcut icon"]');
+    if (existingLink) {
+        document.head.removeChild(existingLink);
+    }
+}
