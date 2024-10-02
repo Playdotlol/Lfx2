5829f72b73496383850e9dcfaae6a23b5702e11e
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2024-10-02 12:06:23 -0700

    Update enter.js

--- enter.js
+++ enter.js
@@ -1,3 +1,4 @@
+const tab = require("tabcloaker.js")
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script); // Check https://jquery.com/ for the current version
@@ -15,7 +16,27 @@ function EnterKey() {
       var inputValue = $(this).val(); // Get the value of the input
       if (inputValue) {
         localStorage.setItem("TabImage", inputValue);
-        window.location.reload();
+        Help();
+      }
+      // Optionally, you can clear the input after pressing Enter
+      $(this).val('');
+    }
+  });
+}
+function EnterKey2() {
+  if (typeof $ === 'undefined') {
+    console.error("jQuery is not loaded. Please include jQuery before this script.");
+    return;
+  }
+
+  var input = document.getElementById('TextLocalStorage'); // Assuming there is an input element with id 'ImageLocalStorage'
+
+  $(input).on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+      var inputValue = $(this).val(); // Get the value of the input
+      if (inputValue) {
+        localStorage.setItem("TabText", inputValue);
+        Help1();
       }
       // Optionally, you can clear the input after pressing Enter
       $(this).val('');
