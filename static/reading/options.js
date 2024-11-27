883477f26aad2c22d62e585c29578a0959279eda
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2024-11-26 18:57:15 -0800

    Create options.js

---
+++ static/reading/options.js
@@ -0,0 +1,32 @@
+function refreshIframePRX() {
+    const proxyIframe = document.getElementById("proxyIframe");
+    proxyIframe.src = proxyIframe.contentWindow.location.href;
+}
+
+
+function toggleFullscreenPRX() {
+    const iframe = document.getElementById("proxyIframe");
+
+    if (!document.fullscreenElement) {
+
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) {
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) { 
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) { 
+            iframe.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
