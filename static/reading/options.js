5916b999eb23a18e9579ed1e4efaa6cf371d595d
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2024-12-14 08:48:28 -0800

    Update options.js

--- static/reading/options.js
+++ static/reading/options.js
@@ -1,3 +1,30 @@
+const Proxy = document.getElementById("proxyIframe");
+const iframe = document.getElementById("game-iframe");
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) {
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) {
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) {
+            iframe.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { 
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
+
+
 function refreshIframePRX() {
     const proxyIframe = document.getElementById("proxyIframe");
     proxyIframe.src = proxyIframe.contentWindow.location.href;
@@ -29,6 +56,11 @@ function toggleFullscreenPRX() {
             document.msExitFullscreen();
         }
     }
+}
+
+
+function refreshIframe() {
+    iframe.src = iframe.contentWindow.location.href;
 }
         function goBack() {
             Proxy.contentWindow.history.back()
