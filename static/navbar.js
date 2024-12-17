a33e6e7c32fdde0426278bdd1e88dba56c1d8a3e
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2024-12-17 12:45:30 -0800

    Update navbar.js

--- static/navbar.js
+++ static/navbar.js
@@ -166,9 +166,6 @@ function insertNavbar() {
     .dropdown:hover .dropdown-content {
       display: block;
     }
-    .settings-card p{
-    float: center;
-    font-size: 35px;}
   </style>
 
   <div id="contextMenu" class="context-menu" style="display:none">
