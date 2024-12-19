ac5b4d774f52cd3287a6394cda12f323f67b6664
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2024-12-18 19:26:28 -0800

    Update tabcloaker.js

--- static/tabcloaker.js
+++ static/tabcloaker.js
@@ -52,12 +52,9 @@ const body = document.body;
 if (backURL) {
     body.style.backgroundImage = `url('${backURL}')`;
     body.classList.add("list");
-} else {
-    body.style.backgroundImage = "url('/static/images/equinox.webp')";
-}
-
-// Ensure the background does not repeat
+    // Ensure the background does not repeat
 body.style.backgroundRepeat = "no-repeat";
+}
 }
 function cloakAll()  {
     Help1();
